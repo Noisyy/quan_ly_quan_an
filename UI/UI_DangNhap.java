1d9f09751cce3dc1f0def54221c814d199156c71
Noisyy <40917428+Noisyy@users.noreply.github.com>
2021-03-28 12:01:55 +0700

    up

---
+++ UI/UI_DangNhap.java
@@ -0,0 +1,11 @@
+import javafx.*;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch (args); 
+    }
+    public void start (Stage primaryStage) throws Exception {
+        primaryStage.setTitle ("Hi Guys")
+    }
+}
