4e31c8e2ff473d96b4005a749fb1eef09f298dd1
Noisyy <40917428+Noisyy@users.noreply.github.com>
2021-05-09 15:31:26 +0700

    update

---
+++ src/Form/ThongKe.java
@@ -0,0 +1,13 @@
+package Form;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ThongKe {
+    @FXML
+    private Button bt_luu;
+    @FXML
+    void Click_bt_luu(MouseEvent event) {
+        exit();
+    }
+}
