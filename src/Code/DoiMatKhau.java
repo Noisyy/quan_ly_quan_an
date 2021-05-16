dc700afad8aeabf078d5bba9d3c872f45abcbc8a
Noisyy <40917428+Noisyy@users.noreply.github.com>
2021-05-16 18:29:08 +0700

    update

--- src/Code/DoiMatKhau.java
+++ src/Code/DoiMatKhau.java
@@ -1,3 +1,36 @@
+package Form;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class DoiMatKhau {
-    
+
+    @FXML
+    private TextField tf_TaiKhoan;
+
+    @FXML
+    private TextField tf_MatKhauCu;
+
+    @FXML
+    private TextField tf_MatKhauMoi;
+
+    @FXML
+    private TextField tf_NhapLaiMatKhauMoi;
+
+    @FXML
+    private Button bt_XacNhan;
+
+    @FXML
+    private Button bt_Thoat;
+
+    @FXML
+    void Click_bt_Thoat(MouseEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    void Click_bt_XacNhan(MouseEvent event) {
+
+    }
 }
--- src/Code/QuanLyThucDon.java
+++ src/Code/QuanLyThucDon.java
@@ -9,6 +9,15 @@ import javafx.scene.control.TextField;
 public class QuanLyThucDon {
 
     @FXML
+    private Tab tab_QLLTD;
+
+    @FXML
+    private Tab tab_QLGM;
+
+    @FXML
+    private Tab tab_QLTD;
+
+    @FXML
     private TextField tf_MaLoai_QLLTD;
 
     @FXML
@@ -98,86 +107,115 @@ public class QuanLyThucDon {
     @FXML
     private TextField tf_SoLuong_QLGM;
 
-    public void AnHienNutLuu(Boolean CongTac){
+    public void AnHienNut_QLLTD(Boolean CongTac){
         bt_Luu_QLLTD.setVisible(CongTac);
-        bt_Luu_QLGM.setVisible(CongTac);
-        bt_Luu_QLTD.setVisible(CongTac);
+        bt_Xoa_QLLTD.setVisible(CongTac);
+        bt_Them_QLLTD.setVisible(CongTac);
+        bt_Sua_QLLTD.setVisible(!CongTac);
     }
     
+    public void AnHienNut_QLTD(Boolean CongTac){
+        bt_Luu_QLTD.setVisible(CongTac);
+        bt_Xoa_QLTD.setVisible(CongTac);
+        bt_Them_QLTD.setVisible(CongTac);
+        bt_Sua_QLTD.setVisible(!CongTac);
+    }
+
+        public void AnHienNut_QLGM(Boolean CongTac){
+        bt_Luu_QLGM.setVisible(CongTac);
+        bt_Xoa_QLGM.setVisible(CongTac);
+        bt_Them_QLGM.setVisible(CongTac);
+        bt_Sua_QLGM.setVisible(!CongTac);
+    }
+
     @FXML
     void Click_bt_Luu_QLGM(MouseEvent event) {
-
+        AnHienNut_QLGM(false);
     }
 
     @FXML
     void Click_bt_Luu_QLLTD(MouseEvent event) {
-
+        AnHienNut_QLLTD(false);
     }
 
     @FXML
     void Click_bt_Luu_QLTD(MouseEvent event) {
-
+        AnHienNut_QLTD(false);
     }
 
     @FXML
     void Click_bt_Sua_QLLTD(MouseEvent event) {
-        AnHienNutLuu(false);
+        AnHienNut_QLLTD(true);
     }
 
     @FXML
     void Click_bt_Sua_QLTD(MouseEvent event) {
-        AnHienNutLuu(false);
+        AnHienNut_QLTD(true);
     }
 
     @FXML
     void Click_bt_Sua_QLGM(MouseEvent event){
-        AnHienNutLuu(false);
+        AnHienNut_QLGM(true);
     }
     
     @FXML
     void Click_bt_Them_QLGM(MouseEvent event) {
-        AnHienNutLuu(false);
+        AnHienNut_QLGM(false);
     }
 
     @FXML
     void Click_bt_Them_QLLTD(MouseEvent event) {
-        AnHienNutLuu(false);
+        AnHienNut_QLLTD(false);
     }
 
     @FXML
     void Click_bt_Them_QLTD(MouseEvent event) {
-        AnHienNutLuu(false);
+        AnHienNut_QLTD(false);
     }
 
     @FXML
     void Click_bt_Thoat_QLGM(MouseEvent event) {
-        System.exit(0);
+        AnHienNut_QLGM(false);
     }
 
     @FXML
     void Click_bt_Thoat_QLLTD(MouseEvent event) {
-        System.exit(0);
+        AnHienNut_QLLTD(false);
     }
 
     @FXML
     void Click_bt_Thoat_QLTD(MouseEvent event) {
-        System.exit(0);
+        AnHienNut_QLTD(false);
     }
 
     @FXML
     void Click_bt_Xoa_QLGM(MouseEvent event) {
-
+        AnHienNut_QLGM(false);
     }
 
     @FXML
     void Click_bt_Xoa_QLLTD(MouseEvent event) {
-
+        AnHienNut_QLLTD(false);
     }
 
     @FXML
     void Click_bt_Xoa_QLTD(MouseEvent event) {
+        AnHienNut_QLTD(false);
+    }
+
+        @FXML
+    void Selection_QLTD(ActionEvent event) {
 
     }
 
+        @FXML
+    void Selection_QLLTD(ActionEvent event) {
+
+    }
+
+        @FXML
+    void Selection_QLGM(ActionEvent event) {
+
+    }
 }
 
